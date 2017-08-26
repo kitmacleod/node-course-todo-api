cb30ba100f41dbef1d61c35fe60ec01832cad73a
Kit Macleod <kit.macleod@gmail.com>
2017-08-26 07:15:35 +0100

    Update scipt

---
+++ playground/mongodb-update.js
@@ -0,0 +1,40 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err,db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  // db.collection('Todos').findOneAndUpdate({
+  //   _id: new ObjectID('59a10be97d139bde903118c0')
+  // }, {
+  //   $set: {
+  //     completed: true
+  //   }
+  // }, {
+  //   returnOriginal: false
+  // }).then((result) => {
+  //   console.log(result);
+  // });
+
+db.collection('Users').findOneAndUpdate({
+  _id: new ObjectID('59a0ee366ef9331d4cadb292')
+}, {
+  $inc: {age: 1}
+}, {
+  returnOriginal: false
+}).then((result) => {
+  console.log(result);
+});
+
+
+
+
+
+
+
+
+// db.close();
+});
